7f0d7f5d51c7754a62fb7b4c0e62cadd60dabcdb
Vadym Shved <vadym.shved@consultant.turkcell.com.tr>
2024-07-29 12:16:01 +0200

    added autostarter for workaround with action boot event

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // some OEMs have restrictions for boot event broadcasts
+    implementation(libs.autostarter)
 }
